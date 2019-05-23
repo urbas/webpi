eaa857539987b92ac1c1133f070cdbfc75f9f284
Matej Urbas <matej.urbas@gmail.com>
2019-05-23 08:20:58 +0100

    Formatting build step now prints a better error message.

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -5,15 +5,13 @@ import App from "./App";
 describe("<App>", function() {
   it("should contain welcome message", () => {
     const wrapper = shallow(<App />);
-    expect(
-      wrapper.contains(<h1 className="App-title">webpi</h1>)
-    ).toEqual(true);
+    expect(wrapper.contains(<h1 className="App-title">webpi</h1>)).toEqual(
+      true
+    );
   });
 
   it("should contain the health message", () => {
     const wrapper = shallow(<App />);
-    expect(
-      wrapper.contains(<div>Health: false</div>)
-    ).toEqual(true);
+    expect(wrapper.contains(<div>Health: false</div>)).toEqual(true);
   });
 });
