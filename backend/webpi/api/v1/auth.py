7a7ac649ac857dcf2adc4d43299d8dcf617d117c
Matej Urbas <matej.urbas@gmail.com>
2019-07-23 10:24:17 +0100

    Login function code now more readable

--- backend/webpi/api/v1/auth.py
+++ backend/webpi/api/v1/auth.py
@@ -16,14 +16,18 @@ def get_user():
 def login():
     if request.json is None:
         return abort(400)
+
     email = request.json.get("email")
     password = request.json.get("password")
     if email is None or password is None:
         return abort(400)
+
     user = current_app.config.users.get(email)
     if user is None:
         return abort(400)
-    if password == user.get("password"):
-        login_user(User(email))
-        return jsonify({})
-    return abort(400)
+
+    if password != user.get("password"):
+        return abort(400)
+
+    login_user(User(email))
+    return jsonify({})
