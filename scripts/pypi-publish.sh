f56619658214b520c7d0841edc699a9967291cd9
Matej Urbas <matej.urbas@gmail.com>
2020-02-09 14:02:20 +0000

    travis: pypi publish script takes TRAVIS_EXTRA_ARGS through CLI args

--- scripts/pypi-publish.sh
+++ scripts/pypi-publish.sh
@@ -11,4 +11,4 @@ LATEST_RELEASE_TAG=$(git describe --tags '--match=v*' --abbrev=0)
 cd $SRC_DIR/backend
 
 docker build -t webpi-backend:latest --build-arg PYTHON_VERSION=$(cat ../.python-version) .
-docker run --env=WEBPI_VERSION=${LATEST_RELEASE_TAG#v} webpi-backend:latest bash -c "pip install --user --upgrade setuptools wheel twine && python setup.py sdist bdist_wheel && python -m twine upload --non-interactive -u __token__ -p $TWINE_PASSWORD --skip-existing $TWINE_EXTRA_ARGS dist/*"
+docker run --env=WEBPI_VERSION=${LATEST_RELEASE_TAG#v} webpi-backend:latest bash -c "pip install --user --upgrade setuptools wheel twine && python setup.py sdist bdist_wheel && python -m twine upload --non-interactive -u __token__ -p $TWINE_PASSWORD --skip-existing $* dist/*"
